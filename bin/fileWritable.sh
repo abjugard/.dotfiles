606c85860337d28fc23e4b4b56f282c3cc6cae4a
Adrian Bjugård <adrian@qng.se>
2014-03-07 11:49:52 +0100

    hej

--- bin/fileWritable.sh
+++ bin/fileWritable.sh
@@ -2,18 +2,26 @@
 PROG="$1"
 FILE="$2"
 
+FILES="${*:2}"
+
 [ $# -eq 1 ] && $PROG
 
 [ $# -ne 2 ] && exit 1
 
 if [[ -w "$FILE" || -w `dirname $FILE` && ! -f "$FILE" ]]
 then
-	$PROG $FILE
+	$PROG $FILES
 else
 	read -n1 -p "No write permissions for $2, sudo all up in that motherfucker? " yn
 	case $yn in
-		Y|y ) sudo $PROG $@;;
-		^C|^c ) exit;;
-		* ) $PROG $FILE;;
+		Y|y ) 
+			echo ""
+			sudo $PROG $FILES;;
+		^C|^c ) 
+			echo ""
+			exit;;
+		* ) 
+			echo ""
+			$PROG $FILES;;
 	esac
 fi
