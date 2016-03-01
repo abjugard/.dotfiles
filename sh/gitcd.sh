026363649d6a2da8ba95063fe49bd492c697aa33
Adrian Bjugård <adrian@qng.se>
2016-03-01 20:38:38 +0100

    more compatible gitcd command

--- sh/gitcd.sh
+++ sh/gitcd.sh
@@ -1,5 +1,5 @@
 gitcd() {
-    if [[ $(which git 2> /dev/null) && $(git status 2>/dev/null) ]]
+    if [[ -n $(which git 2> /dev/null) && -n $(git status 2>/dev/null) ]]
     then
         _GROOT=$(git rev-parse --show-toplevel)
         if [[ -z "$1" && "$_GROOT" != "$PWD" ]]
