83f97fb4310f80f056e27425b4fd8def078b215d
Adrian Bjugård <adrian.bjugard@omegapoint.se>
2022-02-03 14:06:27 +0100

    Improve gitcd, should be much faster on all OS, especially Windows

--- sh/gitcd.sh
+++ sh/gitcd.sh
@@ -1,5 +1,5 @@
 gitcd() {
-    if [[ -n $(which git 2> /dev/null) && -n $(git status 2>/dev/null) ]]
+    if [[ -n $(git_repo_name 2>/dev/null) ]]
     then
         _GROOT=$(git rev-parse --show-toplevel)
         if [[ -z "$1" && "$_GROOT" != "$PWD" ]]
